a4277f812a0526a8d8b7aae2f3646858888728d8
Garret <gwiats@gmail.com>
2021-03-21 18:08:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(list, names) {
+    let matching = list.filter(check => check.toUpperCase() === names.toUpperCase())
+    return matching
+}
+
+function fuzzyMatch(list, names) {
+    let matching = list.filter(check => check.substring(0,2) === names)
+    return matching
+}
+
+function matchName(group, names) {
+    let filtered = group.filter(check => check.name === names)
+    return filtered
+}
